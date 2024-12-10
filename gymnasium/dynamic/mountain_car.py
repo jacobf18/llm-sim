42ad23989c42761938628eba8e7f09e3587a6fde
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2024-12-10 00:55:31 -0500

    added empty dynamic mountain_car.py file

---
+++ gymnasium/dynamic/mountain_car.py
@@ -0,0 +1,4 @@
+# TODO: Write a dynamic environment for the mountain car domain.
+# The gravity of the environment will change drastically after 100 steps.
+# We can set this level of drastic change, but the changes will not be continuous.
+# We can include 3 levels of changes: low, medium, and high.
