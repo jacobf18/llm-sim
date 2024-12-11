5366c59cadc7eac83be3662a564a245350af1511
Jacob Feitelberg <j.e.feitelberg@gmail.com>
2024-12-10 20:20:17 -0500

    added dynamic mountain car where gravity changes from low to medium to high

--- gymnasium/dynamic/mountain_car.py
+++ gymnasium/dynamic/mountain_car.py
@@ -2,3 +2,31 @@
 # The gravity of the environment will change drastically after 100 steps.
 # We can set this level of drastic change, but the changes will not be continuous.
 # We can include 3 levels of changes: low, medium, and high.
+
+from gymnasium.envs.classic_control.mountain_car import MountainCarEnv
+
+class DynamicMountainCarEnv(MountainCarEnv):
+    def __init__(self, gravity=0.0025, low_gravity=0.0005, medium_gravity=0.0025, high_gravity=0.25, **kwargs):
+        super(DynamicMountainCarEnv, self).__init__(**kwargs)
+        self.gravity = low_gravity
+        self.low_gravity = low_gravity
+        self.medium_gravity = medium_gravity
+        self.high_gravity = high_gravity
+        self.steps = 0
+
+    def step(self, action, **kwargs):
+        self.steps += 1
+        if self.steps == 100:
+            print('Low gravity')
+            self.gravity = self.low_gravity
+        elif self.steps == 200:
+            print('Medium gravity')
+            self.gravity = self.medium_gravity
+        elif self.steps == 300:
+            print('High gravity')
+            self.gravity = self.high_gravity
+        return super(DynamicMountainCarEnv, self).step(action, **kwargs)
+
+    def reset(self, **kwargs):
+        self.steps = 0
+        return super(DynamicMountainCarEnv, self).reset(**kwargs)
--- gymnasium/mountain-car.py
+++ gymnasium/mountain-car.py
@@ -1,14 +1,22 @@
 import gymnasium as gym
+from dynamic.mountain_car import DynamicMountainCarEnv
 
 description = """
 The Mountain Car MDP is a deterministic MDP that consists of a car placed stochastically at the bottom of a sinusoidal valley, with the only possible actions being the accelerations that can be applied to the car in either direction. The goal of the MDP is to strategically accelerate the car to reach the goal state on top of the right hill. There are two versions of the mountain car domain in gymnasium: one with discrete actions and one with continuous. This version is the one with continuous actions.
 """
 
+# Register the dynamic environment
+gym.register(
+    id="dynamic/DynamicMountainCar-v0",
+    entry_point=DynamicMountainCarEnv,
+    max_episode_steps=1000
+)
+
 # The dynamics of the system are defined by the following equations:
 # velocity_{t+1} = velocity_t + (action - 1) * force - cos(3 * position_t) * gravity
 # position_{t+1} = position_t + velocity_{t+1}
 
-env = gym.make("MountainCar-v0", render_mode="human")
+env = gym.make("dynamic/DynamicMountainCar-v0", render_mode="human")
 
 observation, info = env.reset()
 
